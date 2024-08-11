b51cf8ac9238b7a60fd79b59f7f4e05744148047
MichaelLevin5908 <michaellevin42@g.ucla.edu>
2024-08-11 13:33:17 -0700

    changed colors for the application

--- clinic_finder/clinic_finder/lib/common/utils/colors.dart
+++ clinic_finder/clinic_finder/lib/common/utils/colors.dart
@@ -1,15 +1,17 @@
 import 'package:flutter/material.dart';
+
 const Map<int, Color> primarySwatch = {
-  50: Color.fromRGBO(255, 207, 68, .1),
-  100: Color.fromRGBO(255, 207, 68, .2),
-  200: Color.fromRGBO(255, 207, 68, .3),
-  300: Color.fromRGBO(255, 207, 68, .4),
-  400: Color.fromRGBO(255, 207, 68, .5),
-  500: Color.fromRGBO(255, 207, 68, .6),
-  600: Color.fromRGBO(255, 207, 68, .7),
-  700: Color.fromRGBO(255, 207, 68, .8),
-  800: Color.fromRGBO(255, 207, 68, .9),
-  900: Color.fromRGBO(255, 207, 68, 1),
+  50: Color.fromRGBO(68, 138, 255, .1),
+  100: Color.fromRGBO(68, 138, 255, .2),
+  200: Color.fromRGBO(68, 138, 255, .3),
+  300: Color.fromRGBO(68, 138, 255, .4),
+  400: Color.fromRGBO(68, 138, 255, .5),
+  500: Color.fromRGBO(68, 138, 255, .6),
+  600: Color.fromRGBO(68, 138, 255, .7),
+  700: Color.fromRGBO(68, 138, 255, .8),
+  800: Color.fromRGBO(68, 138, 255, .9),
+  900: Color.fromRGBO(68, 138, 255, 1),
 };
-const MaterialColor primaryColor = MaterialColor(0xFFFFCF44, primarySwatch);
-const int primaryColorDark = 0xFFFD9725;
+
+const MaterialColor primaryColor = MaterialColor(0xFF448AFF, primarySwatch);
+const int primaryColorDark = 0xFF2979FF;

d88d945b8f6fc1d22558ef383f3e01a9e02c3010
MichaelLevin5908 <michaellevin42@g.ucla.edu>
2024-08-21 21:39:44 -0700

    encapsulates sign out

--- clinic_finder/clinic_finder/lib/features/clinic/UI/clinicslist/clinicslistpage.dart
+++ clinic_finder/clinic_finder/lib/features/clinic/UI/clinicslist/clinicslistpage.dart
@@ -1,30 +1,36 @@
+import 'package:amplify_flutter/amplify_flutter.dart';
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
 import 'package:clinic_finder/common/utils/colors.dart' as constants;
+import 'package:go_router/go_router.dart';
 import 'package:clinic_finder/common/navigation/router/routes.dart';
 
-import 'package:amplify_flutter/amplify_flutter.dart';
+class ClinicsListPage extends StatefulWidget {
+  const ClinicsListPage({super.key});
 
-class ClinicsListPage extends StatelessWidget {
-  const ClinicsListPage({
-    super.key,
-  });
+  @override
+  ClinicsListPageState createState() => ClinicsListPageState();
+}
+
+class ClinicsListPageState extends State<ClinicsListPage> {
+  Future<void> _signOut() async {
+    await Amplify.Auth.signOut(); 
+    if (!mounted) 
+    {
+      return;
+    }
+    context.goNamed(AppRoute.home.name);
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
-        title: const Text(
-          'Clinics Screen',
-        ),
+        title: const Text('Clinics Screen'),
         backgroundColor: const Color(constants.primaryColorDark),
         leading: IconButton(
           icon: const Icon(Icons.arrow_back),
-          onPressed: () async {
-            await Amplify.Auth.signOut();  // Sign out the user
-            context.goNamed(AppRoute.home.name); // Navigate back to the sign-in screen
-          },
+          onPressed: _signOut,
         ),
       ),
       floatingActionButton: FloatingActionButton(
@@ -38,3 +44,4 @@ class ClinicsListPage extends StatelessWidget {
     );
   }
 }
+

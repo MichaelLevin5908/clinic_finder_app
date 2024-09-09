111f226a04e8ecab72abe5a0202229ac86cae67a
MichaelLevin5908 <michaellevin42@g.ucla.edu>
2024-09-08 21:47:17 -0700

    added markers

--- clinic_finder/clinic_finder/lib/features/clinic/UI/clinicslist/clinicslistpage.dart
+++ clinic_finder/clinic_finder/lib/features/clinic/UI/clinicslist/clinicslistpage.dart
@@ -1,11 +1,12 @@
 import 'package:amplify_flutter/amplify_flutter.dart';
+import 'package:clinic_finder/features/clinic/UI/clinicslist/yelp_service.dart';
 import 'package:flutter/material.dart';
 import 'package:clinic_finder/common/utils/colors.dart' as constants;
 import 'package:flutter_map/flutter_map.dart';
 import 'package:go_router/go_router.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:clinic_finder/common/navigation/router/routes.dart';
-import 'package:latlong2/latlong.dart';  
+import 'package:latlong2/latlong.dart';
 import 'package:geolocator/geolocator.dart';
 
 class ClinicsListPage extends StatefulWidget {
@@ -17,6 +18,12 @@ class ClinicsListPage extends StatefulWidget {
 
 class ClinicsListPageState extends State<ClinicsListPage> {
   LatLng? _userLocation;
+  List<Marker> _markers = []; // Initialize an empty markers list
+
+  String clinicName = '';
+  String phoneNumber = '';
+  String address = '';
+  String hours = '';
 
   Future<void> _signOut() async {
     await Amplify.Auth.signOut();
@@ -49,22 +56,84 @@ class ClinicsListPageState extends State<ClinicsListPage> {
         return Future.error('Location permissions are denied');
       }
     }
-    
+
     if (permission == LocationPermission.deniedForever) {
       return Future.error('Location permissions are permanently denied.');
     }
 
-    Position position = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
+    Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high);
 
     setState(() {
       _userLocation = LatLng(position.latitude, position.longitude);
     });
   }
 
+  Future<void> _loadClinicData() async {
+    final location = _userLocation != null ? _userLocation.toString() : '';
+    var hospitalData = [];
+    try {
+      final hospitalList = await fetchHospitalData(location);
+      print('API Response: $hospitalList');
+      hospitalData = hospitalList;
+    } catch (e) {
+      print('Error: $e');
+    }
+
+    // Retrieves the first clinic in the json file
+    setState(() {
+      if (hospitalData.isNotEmpty) {
+        // 0 is name, 1 is address, 2 is phone number, 3 is hours
+        clinicName = hospitalData[0][0];
+        address = hospitalData[0][1];
+        phoneNumber = hospitalData[0][2];
+        hours = hospitalData[0][3];
+      } else {
+        clinicName = "Error";
+        address = "Error";
+        phoneNumber = "Error";
+        hours = "Error";
+      }
+    });
+
+    _addMarkers(hospitalData); // Call to add markers after loading data
+  }
+
+Future<void> _addMarkers(List<dynamic> hospitalData) async {
+  if (_userLocation != null) {
+    setState(() {
+      _markers = hospitalData.map((hospital) {
+        final latitude = double.tryParse(hospital[4].toString()) ?? 0.0;
+        final longitude = double.tryParse(hospital[5].toString()) ?? 0.0;
+
+        return Marker(
+          width: 80.0,
+          height: 80.0,
+          point: LatLng(latitude, longitude),
+          builder: (ctx) => GestureDetector(
+            onTap: () {
+              _map(); // Define what happens on tap
+            },
+            child: Icon(
+              Icons.location_pin, // Display a pin icon
+              color: Colors.red,
+              size: 40.0,
+            ),
+          ),
+        );
+      }).toList();
+    });
+  }
+}
+
   @override
   void initState() {
     super.initState();
-    _getUserLocation();
+    _getUserLocation().then((_) {
+      if (_userLocation != null) {
+        _loadClinicData();
+      }
+    });
   }
 
   @override
@@ -90,7 +159,7 @@ class ClinicsListPageState extends State<ClinicsListPage> {
         actions: [
           IconButton(
             icon: const Icon(Icons.info, color: Colors.white),
-            onPressed: _map, 
+            onPressed: _map,
           ),
         ],
       ),
@@ -104,12 +173,12 @@ class ClinicsListPageState extends State<ClinicsListPage> {
           : Center(
               child: FlutterMap(
                 options: MapOptions(
-                  initialCenter: _userLocation!,
-                  initialZoom: 11,
+                  center: _userLocation!,
+                  zoom: 11,
                 ),
                 children: [
                   openStreetMapTileLayer,
-                  //MarkerLayer(markers: markers) // Add markers if needed.
+                  MarkerLayer(markers: _markers),
                 ],
               ),
             ),
@@ -118,8 +187,9 @@ class ClinicsListPageState extends State<ClinicsListPage> {
 }
 
 TileLayer get openStreetMapTileLayer => TileLayer(
-  urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
-  userAgentPackageName: 'com.yourcompany.clinic_finder',
-);
+      urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+      userAgentPackageName: 'com.yourcompany.clinic_finder',
+    );
+
 
 
--- clinic_finder/clinic_finder/lib/features/clinic/UI/clinicslist/clinicsmappage.dart
+++ clinic_finder/clinic_finder/lib/features/clinic/UI/clinicslist/clinicsmappage.dart
@@ -3,6 +3,7 @@ import 'package:amplify_flutter/amplify_flutter.dart';
 import 'package:flutter/material.dart';
 import 'package:clinic_finder/common/utils/colors.dart' as constants;
 import 'package:flutter_map/flutter_map.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:go_router/go_router.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:clinic_finder/common/navigation/router/routes.dart';
@@ -10,7 +11,6 @@ import 'package:latlong2/latlong.dart';
 import 'package:path_provider/path_provider.dart';
 import 'dart:io';
 import 'yelp_service.dart';
-
 
 class ClinicsMapPage extends StatefulWidget {
   const ClinicsMapPage({super.key});
@@ -29,35 +29,6 @@ class ClinicsMapState extends State<ClinicsMapPage> {
   @override
   void initState() {
     super.initState();
-    _loadClinicData();
-  }
-
-  Future<void> _loadClinicData() async {
-    const location = 'Los Angeles, CA';
-    var hospitalData = [];
-    try {
-      final hospitalList = await fetchHospitalData(location);
-      print('API Response: $hospitalList');
-      hospitalData = hospitalList;
-    } catch (e) {
-      print('Error: $e');
-    }
-
-    // Retrieves the first clinic in the json file
-    setState(() {
-      if(hospitalData.isNotEmpty){
-        // 0 is name, 1 is address, 2 is phone number, 3 is hours
-        clinicName =   hospitalData[0][0];
-        address =      hospitalData[0][1];
-        phoneNumber =  hospitalData[0][2];
-        hours =        hospitalData[0][3];
-      } else {
-        clinicName =  "Error";
-        address =     "Error";
-        phoneNumber = "Error";
-        hours =       "Error";
-      }
-    });
   }
 
   Future<void> _signOut() async {
